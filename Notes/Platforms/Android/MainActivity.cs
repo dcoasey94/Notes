241fcac29ab654c5c6f7ed42efbe8ce3fdac202f
dcoasey94 <coaseydarrell@gmail.com>
2023-11-18 20:15:19 -0500

    bugged chartboost integration, comp. error

--- Notes/Platforms/Android/MainActivity.cs
+++ Notes/Platforms/Android/MainActivity.cs
@@ -1,11 +1,18 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using com.chartboost.sdk;
+using com.chartboost.sdk.ads;
 
 namespace Notes
 {
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+		protected override void OnCreate(Bundle savedInstanceState)
+{
+    base.OnCreate(savedInstanceState);
+    Chartboost.Start("6556de4202db1dc1f4910d49", "715763ec356bcd27961c9a06f9f683a9168aaaf2");
+}
     }
 }
